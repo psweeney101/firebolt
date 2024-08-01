de5cafc29a582a3092f247d1c9f35cf6438dcb66
Patrick Sweeney <33041355+psweeney101@users.noreply.github.com>
2024-08-01 21:36:00 +0000

    Add Nx

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -2,3 +2,6 @@
 
 # Have git mark this repository as safe
 git config --global --add safe.directory ${PWD}
+
+# Install nx globally
+npm install --global nx
---
+++ apps/ui/src/test-setup.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'jest-preset-angular/setup-jest';
+// @ts-expect-error https://thymikee.github.io/jest-preset-angular/docs/getting-started/test-environment
+globalThis.ngJest = {
+  testEnvironmentOptions: {
+    errorOnUnknownElements: true,
+    errorOnUnknownProperties: true,
+  },
+};
